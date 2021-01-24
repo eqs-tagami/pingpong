ebc3f6f50e598172fb41085e3f1a8a63f09808b8
田上翔斗 <shoto@stagami.local>
2021-01-24 20:09:20 +0900

    create loop method

--- js/main.js
+++ js/main.js
@@ -5,6 +5,23 @@
     constructor (canvas) {
       this.canvas = canvas;
       this.ctx = this.canvas.getContext('2d');
+      this.loop();
+    }
+
+    loop() {
+      this.update();
+      this.draw();
+
+      requestAnimationFrame(() => {
+        this.loop();
+      });
+    }
+
+    update() {
+    }
+
+    draw() {
+      console.log(new Date());
     }
   }
 
